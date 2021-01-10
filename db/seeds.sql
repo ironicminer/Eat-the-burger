a96caba218769cd7d79bc395d6b0fe0fe41692ce
nate <rotarygurter@gmail.com>
2021-01-09 19:45:00 -0600

    added seed

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Tofu');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Cheeseburger', true);
